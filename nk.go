15309ffc309c333c2e53b93dc57afceff05001b4
Antonio Ojea <aojea@redhat.com>
2021-06-03 16:55:55 +0200

    add TCP listen

--- nk.go
+++ nk.go
@@ -53,11 +53,13 @@ func init() {
 	flag.BoolVar(&flagListen, "listen", false, "Bind and listen for incoming connections")
 	flag.BoolVar(&flagUDP, "udp", false, "Use UDP instead of default TCP")
 	flag.BoolVar(&flagDebug, "debug", false, "Debug")
-	flag.IntVar(&flagSrcPort, "source-port", 0, "Specify source port to use")
+	flag.IntVar(&flagSrcPort, "source-port", 0, "Specify source port to use on connections")
 	flag.StringVar(&flagInterface, "interface", "", "Specify interface to use. Default interface with default route")
 
 	flag.Usage = func() {
-		fmt.Fprint(os.Stderr, "Usage: nk [options] [hostname] [port]\n\n")
+		fmt.Fprint(os.Stderr, "Usage: nk [options] [hostname] [port]\n\n"+
+			"In connect mode, the hostname and port arguments tell what to connect.\n"+
+			"In listen mode, hostname and port control the address the server will bind to.\n\n")
 		flag.PrintDefaults()
 	}
 }
@@ -105,28 +107,34 @@ func main() {
 		}
 	}()
 
-	// run netcat
-	err = netcat(ctx, args)
-	if err != nil {
-		log.Fatalln(err)
+	if len(args) != 2 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if !flagListen {
+		err = connect(ctx, args)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	} else {
+		err = listen(ctx, args)
+		if err != nil {
+			log.Fatalln(err)
+		}
 	}
 	os.Exit(0)
 
 }
 
-func netcat(ctx context.Context, args []string) error {
+// In connect mode, the hostname and port arguments tell what to connect.
+func connect(ctx context.Context, args []string) error {
 	var err error
 	var destIP net.IP
 	var sourceIP net.IP
 	var destPort uint16
 
 	// Validation
-	// In connect mode, the hostname and port arguments tell what to connect.
-	// In listen mode, hostname and port control the address the server will bind to.
-	if len(args) != 2 {
-		flag.Usage()
-		return fmt.Errorf("Wrong number of flags")
-	}
 	ips, err := net.LookupHost(args[0])
 	if err != nil || len(ips) == 0 {
 		return errors.Wrapf(err, "Invalid destination Host: %s", args[0])
@@ -433,37 +441,373 @@ func netcat(ctx context.Context, args []string) error {
 	// Or from a TCP/UDP socket to stdout in server mode
 
 	// client mode: stdin ---> socket(hostname,port)
-	if !flagListen {
-		dest := tcpip.FullAddress{
-			NIC:  nicID,
-			Addr: ipToStackAddress(destIP),
-			Port: destPort,
+	dest := tcpip.FullAddress{
+		NIC:  nicID,
+		Addr: ipToStackAddress(destIP),
+		Port: destPort,
+	}
+	var conn net.Conn
+	log.Printf("Dialing ...")
+	if !flagUDP {
+		ctxConnect, cancelConnect := context.WithTimeout(ctx, 5*time.Second)
+		defer cancelConnect()
+		conn, err = dialTCP(ctxConnect, ipstack, &laddr, &dest, protocolNumber)
+		if err != nil {
+			log.Printf("Dialing error: %s\n", err)
+			return fmt.Errorf("Can't connect to server: %s\n", err)
 		}
-		var conn net.Conn
-		log.Printf("Dialing ...")
-		if !flagUDP {
-			ctxConnect, cancelConnect := context.WithTimeout(ctx, 5*time.Second)
-			defer cancelConnect()
-			conn, err = dialTCP(ctxConnect, ipstack, &laddr, &dest, protocolNumber)
-			if err != nil {
-				log.Printf("Dialing error: %s\n", err)
-				return fmt.Errorf("Can't connect to server: %s\n", err)
-			}
-		} else {
-			conn, err = gonet.DialUDP(ipstack, &laddr, &dest, protocolNumber)
-			if err != nil {
-				return fmt.Errorf("Can't connect to server: %s\n", err)
-			}
+	} else {
+		conn, err = gonet.DialUDP(ipstack, &laddr, &dest, protocolNumber)
+		if err != nil {
+			return fmt.Errorf("Can't connect to server: %s\n", err)
 		}
-		log.Printf("Connection established")
+	}
+	log.Printf("Connection established")
+	errCh := make(chan error, 2)
+	go func() {
+		_, err = io.Copy(conn, os.Stdin)
+		errCh <- err
+	}()
+	go func() {
+		_, err = io.Copy(os.Stdout, conn)
+		errCh <- err
+	}()
+	// the signal handler can unblock this too
+	select {
+	case err = <-errCh:
+		log.Printf("Connection error: %v", err)
+	case <-ctx.Done():
+		log.Printf("Done")
+	}
+	// give a chance to terminate gracefully
+	time.Sleep(500 * time.Millisecond)
+	return err
+}
+
+// In listen mode, hostname and port control the address the server will bind to.
+func listen(ctx context.Context, args []string) error {
+	var err error
+	var localIP net.IP
+	var localPort uint16
+
+	// Validation
+	// In listen mode, hostname and port control the address the server will bind to.
+	localIP = net.ParseIP(args[0])
+	if localIP == nil {
+		return fmt.Errorf("Invalid hostname %s", args[0])
+	}
+
+	intfName, err := getInterfaceFromIP(localIP)
+	if err != nil {
+		return errors.Wrapf(err, "Invalid local IP: %s", args[0])
+	}
+
+	// override the interface if specified
+	if flagInterface != "" {
+		intfName = flagInterface
+	}
+
+	i, err := strconv.Atoi(args[1])
+	localPort = uint16(i)
+	if err != nil || localPort == 0 {
+		return fmt.Errorf("Invalid local Port: %s", args[1])
+	}
+	if flagSrcPort != 0 {
+		return fmt.Errorf("Source port flag only available in connect mode: %d", flagSrcPort)
+	}
+
+	// Defaulting
+
+	// Use TCP by default
+	transportProtocol := tcp.NewProtocol
+	transportProtocolNumber := tcp.ProtocolNumber
+	if flagUDP {
+		transportProtocol = udp.NewProtocol
+		transportProtocolNumber = udp.ProtocolNumber
+	}
+
+	// Use IPv4 or IPv6 depending on the destination address
+	isIPv6 := isIPv6Address(localIP)
+	protocolNumber := ipv4.ProtocolNumber
+	networkProtocol := ipv4.NewProtocol
+	family := netlink.FAMILY_V4
+	if isIPv6 {
+		protocolNumber = ipv6.ProtocolNumber
+		networkProtocol = ipv6.NewProtocol
+		family = netlink.FAMILY_V6
+	}
+
+	mtu, err := rawfile.GetMTU(intfName)
+	if err != nil {
+		return fmt.Errorf("Failed to get interface %s MTU: %v", intfName, err)
+	}
+
+	ifaceLink, err := netlink.LinkByName(intfName)
+	if err != nil {
+		return fmt.Errorf("unable to bind to %q: %v", intfName, err)
+	}
+
+	log.Printf("Creating raw socket")
+	// https://github.com/google/gvisor/blob/108410638aa8480e82933870ba8279133f543d2b/test/benchmarks/tcp/tcp_proxy.go
+	fd, err := unix.Socket(unix.AF_PACKET, unix.SOCK_RAW, int(htons(unix.ETH_P_ALL)))
+	if err != nil {
+		return fmt.Errorf("Could not create socket: %s", err.Error())
+	}
+	defer unix.Close(fd)
+
+	if fd < 0 {
+		return fmt.Errorf("Socket error: return < 0")
+	}
+
+	if err = unix.SetNonblock(fd, true); err != nil {
+		return fmt.Errorf("Error setting fd to nonblock: %s", err)
+	}
+
+	ll := unix.SockaddrLinklayer{
+		Protocol: htons(unix.ETH_P_ALL),
+		Ifindex:  ifaceLink.Attrs().Index,
+		Pkttype:  unix.PACKET_HOST,
+	}
+
+	if err := unix.Bind(fd, &ll); err != nil {
+		return fmt.Errorf("unable to bind to %q: %v", "iface.Name", err)
+	}
+
+	// Add a filter to the socket so we receive only the packets we are interested
+	// TODO we can make this more restrictive with source IP and source Port
+	// xref: https://blog.cloudflare.com/bpf-the-forgotten-bytecode/
+
+	// offset 23 protocol 6 TCP 17 UDP
+	bpfProto := uint32(6)
+	if flagUDP {
+		bpfProto = uint32(17)
+	}
+	bpfFilter := []bpf.Instruction{
+		// check the ethertype
+		bpf.LoadAbsolute{Off: 12, Size: 2},
+		// allow arp
+		bpf.JumpIf{Val: 0x0806, SkipTrue: 10},
+		// check is ipv4
+		bpf.JumpIf{Val: 0x0800, SkipFalse: 10},
+		// check the protocol
+		bpf.LoadAbsolute{Off: 23, Size: 1},
+		bpf.JumpIf{Val: bpfProto, SkipFalse: 8},
+		// check the destination address
+		bpf.LoadAbsolute{Off: 30, Size: 4},
+		bpf.JumpIf{Val: binary.BigEndian.Uint32(localIP.To4()), SkipFalse: 6},
+		// skip if offset non zero
+		bpf.LoadAbsolute{Off: 20, Size: 2},
+		bpf.JumpIf{Cond: bpf.JumpBitsSet, Val: 0x1fff, SkipTrue: 4},
+		// check the destination port
+		bpf.LoadMemShift{Off: 14},
+		bpf.LoadIndirect{Off: 16, Size: 2},
+		bpf.JumpIf{Val: uint32(localPort), SkipFalse: 1},
+		bpf.RetConstant{Val: 0xffff},
+		bpf.RetConstant{Val: 0x0},
+	}
+
+	if isIPv6 {
+		bpfFilter = []bpf.Instruction{
+			// check the ethertype
+			bpf.LoadAbsolute{Off: 12, Size: 2},
+			bpf.JumpIf{Val: 0x86dd, SkipFalse: 14},
+			// check the protocol
+			bpf.LoadAbsolute{Off: 20, Size: 1},
+			// allow icmpv6
+			bpf.JumpIf{Val: 58, SkipTrue: 11},
+			bpf.JumpIf{Val: bpfProto, SkipFalse: 11},
+			// check the destination address
+			bpf.LoadAbsolute{Off: 38, Size: 4},
+			bpf.JumpIf{Val: binary.BigEndian.Uint32(localIP.To16()[0:4]), SkipFalse: 9},
+			bpf.LoadAbsolute{Off: 42, Size: 4},
+			bpf.JumpIf{Val: binary.BigEndian.Uint32(localIP.To16()[4:8]), SkipFalse: 7},
+			bpf.LoadAbsolute{Off: 46, Size: 4},
+			bpf.JumpIf{Val: binary.BigEndian.Uint32(localIP.To16()[8:12]), SkipFalse: 5},
+			bpf.LoadAbsolute{Off: 50, Size: 4},
+			bpf.JumpIf{Val: binary.BigEndian.Uint32(localIP.To16()[12:16]), SkipFalse: 3},
+			// check the destination port
+			bpf.LoadAbsolute{Off: 56, Size: 2},
+			bpf.JumpIf{Val: uint32(localPort), SkipFalse: 1},
+			bpf.RetConstant{Val: 0xffff}, // accept
+			bpf.RetConstant{Val: 0x0},    // drop
+		}
+	}
+	filter, err := bpf.Assemble(bpfFilter)
+	if err != nil {
+		return fmt.Errorf("Failed to generate BPF assembler: %v", err)
+	}
+
+	f := make([]unix.SockFilter, len(filter))
+	for i := range filter {
+		f[i].Code = filter[i].Op
+		f[i].Jf = filter[i].Jf
+		f[i].Jt = filter[i].Jt
+		f[i].K = filter[i].K
+	}
+	fprog := &unix.SockFprog{
+		Len:    uint16(len(filter)),
+		Filter: &f[0],
+	}
+	err = unix.SetsockoptSockFprog(fd, unix.SOL_SOCKET, unix.SO_ATTACH_FILTER, fprog)
+	if err != nil {
+		return fmt.Errorf("unable to set BPF filter on socket: %v", err)
+	}
+
+	log.Printf("Adding ebpf ingress filter on interface %s", ifaceLink.Attrs().Name)
+	// filter on the host so our userspace connections are not resetted
+	// using tc since they are at the beginning of the pipeline
+	// # add an ingress qdisc
+	// tc qdisc add dev eth3 ingress
+	// xref: https://codilime.com/pdf/codilime_packet_flow_in_netfilter_A3-1-1.pdf
+	qdisc := &netlink.GenericQdisc{
+		QdiscAttrs: netlink.QdiscAttrs{
+			LinkIndex: ifaceLink.Attrs().Index,
+			Handle:    netlink.MakeHandle(0xffff, 0),
+			Parent:    netlink.HANDLE_CLSACT,
+		},
+		QdiscType: "clsact",
+	}
+	if err = netlink.QdiscAdd(qdisc); err != nil {
+		return fmt.Errorf("Failed to add qdisc: %v", err)
+	}
+	defer netlink.QdiscDel(qdisc)
+
+	spec, err := loadFilter()
+	if err != nil {
+		return fmt.Errorf("Error creating eBPF program: %v", err)
+	}
+
+	// TODO: IPv6
+	err = spec.RewriteConstants(map[string]interface{}{
+		"PROTO":     uint8(transportProtocolNumber),
+		"IP_FAMILY": uint8(family),
+		// "SRC_IP":    0,
+		"DST_IP": ip2int(localIP),
+		// "SRC_PORT":  0,
+		"DST_PORT": uint16(localPort),
+	})
+	if err != nil {
+		return fmt.Errorf("Error rewriting eBPF program: %v", err)
+	}
+
+	objs := filterObjects{}
+	if err := spec.LoadAndAssign(&objs, nil); err != nil {
+		return fmt.Errorf("failed to load objects: %v", err)
+	}
+	defer objs.Close()
+
+	bpfFd := objs.Ingress.FD()
+	// https://man7.org/linux/man-pages/man8/tc-bpf.8.html
+	ingressFilter := &netlink.BpfFilter{
+		FilterAttrs: netlink.FilterAttrs{
+			LinkIndex: ifaceLink.Attrs().Index,
+			Parent:    netlink.HANDLE_MIN_INGRESS,
+			Handle:    netlink.MakeHandle(0, 1),
+			Protocol:  unix.ETH_P_ALL,
+		},
+		Fd:           bpfFd,
+		Name:         "nkFilter",
+		DirectAction: true,
+	}
+
+	log.Printf("filter %v", ingressFilter.String())
+	if err := netlink.FilterAdd(ingressFilter); err != nil {
+		return fmt.Errorf("Failed to add filter: %v", err)
+	}
+	defer netlink.FilterDel(ingressFilter)
+
+	log.Printf("Creating user TCP/IP stack")
+	// add the socket to the userspace stack
+	la := tcpip.LinkAddress(ifaceLink.Attrs().HardwareAddr)
+
+	linkID, err := fdbased.New(&fdbased.Options{
+		FDs:            []int{fd},
+		MTU:            mtu,
+		EthernetHeader: true,
+		Address:        tcpip.LinkAddress(la),
+		// Enable checksum generation as we need to generate valid
+		// checksums for the veth device to deliver our packets to the
+		// peer. But we do want to disable checksum verification as veth
+		// devices do perform GRO and the linux host kernel may not
+		// regenerate valid checksums after GRO.
+		TXChecksumOffload:  false,
+		RXChecksumOffload:  true,
+		PacketDispatchMode: fdbased.RecvMMsg,
+	})
+	if err != nil {
+		return fmt.Errorf("Can't create user-space link: %v\n", err)
+	}
+
+	if flagDebug {
+		linkID = sniffer.New(linkID)
+	}
+
+	ipstack := stack.New(stack.Options{
+		NetworkProtocols:   []stack.NetworkProtocolFactory{networkProtocol, arp.NewProtocol},
+		TransportProtocols: []stack.TransportProtocolFactory{transportProtocol},
+	})
+	defer func() {
+		ipstack.Close()
+	}()
+
+	if err := ipstack.CreateNIC(1, linkID); err != nil {
+		return fmt.Errorf("Failed to create userspace NIC: %v", err)
+	}
+
+	ipstack.AddAddress(nicID, protocolNumber, ipToStackAddress(localIP))
+	// use the address as source
+	laddr := tcpip.FullAddress{
+		NIC:  nicID,
+		Addr: ipToStackAddress(localIP),
+		Port: uint16(localPort),
+	}
+
+	// Add IPv4 and IPv6 default routes, so all traffic goes through the fake NIC
+	subnet, _ := tcpip.NewSubnet(tcpip.Address(strings.Repeat("\x00", 4)), tcpip.AddressMask(strings.Repeat("\x00", 4)))
+	if isIPv6 {
+		subnet, _ = tcpip.NewSubnet(tcpip.Address(strings.Repeat("\x00", 16)), tcpip.AddressMask(strings.Repeat("\x00", 16)))
+	}
+
+	ipstack.SetRouteTable([]tcpip.Route{
+		{
+			Destination: subnet,
+			NIC:         nicID,
+		},
+	})
+
+	// Implement the netcat logic
+	// It basically copies from a TCP/UDP socket to stdout in server mode
+
+	// server mode: socket(localhost,port) ---> stdin
+	if flagUDP {
+		// TODO: UDP listeners
+	} else {
+		l, err := gonet.ListenTCP(ipstack, laddr, protocolNumber)
+		if err != nil {
+			return fmt.Errorf("Error open TCP listener: %v", err)
+		}
+		defer l.Close()
+		log.Printf("Listening on %s:%d", localIP, localPort)
+		// accept connections
 		errCh := make(chan error, 2)
 		go func() {
-			_, err = io.Copy(conn, os.Stdin)
-			errCh <- err
-		}()
-		go func() {
-			_, err = io.Copy(os.Stdout, conn)
-			errCh <- err
+			inConn, err := l.Accept()
+			if err != nil {
+				errCh <- err
+				return
+			}
+			log.Printf("incoming connection established.")
+
+			// process connection
+			go func() {
+				_, err = io.Copy(inConn, os.Stdin)
+				errCh <- err
+			}()
+			go func() {
+				_, err = io.Copy(os.Stdout, inConn)
+				errCh <- err
+			}()
+
 		}()
 		// the signal handler can unblock this too
 		select {
@@ -472,16 +816,9 @@ func netcat(ctx context.Context, args []string) error {
 		case <-ctx.Done():
 			log.Printf("Done")
 		}
+
 		// give a chance to terminate gracefully
 		time.Sleep(500 * time.Millisecond)
-		return err
-	}
-
-	// server mode: socket(localhost,port) ---> stdin
-	if flagUDP {
-		// TODO: UDP listeners
-	} else {
-
 	}
 	return err
 }
@@ -517,6 +854,32 @@ func getConnectionDetails(dst net.IP) (string, net.IP, net.IP, error) {
 		}
 	}
 	return "", net.IP{}, net.IP{}, fmt.Errorf("failed to get default gateway interface")
+}
+
+func getInterfaceFromIP(localIP net.IP) (string, error) {
+	ifaces, err := net.Interfaces()
+	if err != nil {
+		return "", err
+	}
+	for _, i := range ifaces {
+		addrs, err := i.Addrs()
+		if err != nil {
+			return "", err
+		}
+		for _, addr := range addrs {
+			var ip net.IP
+			switch v := addr.(type) {
+			case *net.IPNet:
+				ip = v.IP
+			case *net.IPAddr:
+				ip = v.IP
+			}
+			if localIP.Equal(ip) {
+				return i.Name, nil
+			}
+		}
+	}
+	return "", fmt.Errorf("%s IP not found in the host", localIP.String())
 }
 
 // htons converts a short (uint16) from host-to-network byte order.
